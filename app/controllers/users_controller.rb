514179d8c5538c1fbc40a87bd0796065d26fcb7d
Spencer Moran <smoran02@gmail.com>
2013-08-02 01:11:14 -0700

    Attempted user odering fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   end
 
   def show
-	  @user = User.find(params[:id])
+	  @user = User.order('id ASC').paginate(page: params[:page])
   end
 
   def new
