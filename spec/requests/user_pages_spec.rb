3aa7fe5a7a30cb5b7f09630ffb80ea32b22fa688
Spencer Moran <smoran02@gmail.com>
2013-08-06 00:12:51 -0700

    Finish follower/following pages

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -49,6 +49,34 @@ describe "UserPages" do
     end
   end
 
+  describe "following/followers" do 
+    let(:user) { FactoryGirl.create(:user) }
+    let(:other_user) { FactoryGirl.create(:user) }
+    before { user.follow!(other_user) }
+
+    describe "followed users" do 
+      before do 
+        sign_in user
+        visit following_user_path(user)
+      end
+
+      it { should have_title(full_title('Following')) }
+      it { should have_selector('h3', text: 'Following') }
+      it { should have_link(other_user.name, href: user_path(other_user)) }
+    end
+
+    describe "followers" do 
+      before do
+        sign_in user
+        visit followers_user_path(other_user)
+      end
+
+      it { should have_title(full_title('Followers')) }
+      it { should have_selector('h3', text: 'Followers') }
+      it { should have_link(user.name, href: user_path(user)) }
+    end
+  end
+
   describe "profile page" do
   	let(:user) { FactoryGirl.create(:user) }
     let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
